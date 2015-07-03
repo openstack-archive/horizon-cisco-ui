c1184ac042e9581e606d0c576e9eed04b037beb9
Rob Cresswell <robert.cresswell@outlook.com>
2015-07-03 11:55:42 +0100

    Initial commit
    This patch adds the core panels, dev install script, basic
    documentation, and testing.
    
    There is a fairly large amount of extra "stuff" left in, due to the
    copy/paste nature of the setup. Specifically, the tests have a lot of
    the horizon test code in which is unecessary, but temporary with the aim
    of getting up and running quickly.
    
    Change-Id: I5fdce186df09e1168e8d52abfec17e0c20a64031
    Partially-Implements: blueprint initial-setup

---
+++ _40_cisco.py
@@ -0,0 +1,10 @@
+# The slug of the dashboard to be added to HORIZON['dashboards']. Required.
+DASHBOARD = 'cisco'
+
+# A list of applications to be added to INSTALLED_APPS.
+ADD_INSTALLED_APPS = [
+    'cisco',
+]
+
+# Extra values to add to the Horizon settings
+UPDATE_HORIZON_CONFIG = {}
