f23f9b70a7558e6c2b2597f16fa2b3007cb7066c
Rob Cresswell <robert.cresswell@outlook.com>
2015-07-03 11:55:42 +0100

    Initial commit
    This patch adds the core panels (Cisco Dashboard and Firewalls),
    dev install script, basic documentation, and testing.
    
    There is a fairly large amount of extra "stuff" left in, due to the
    copy/paste nature of the setup. Specifically, the tests have a lot of
    the horizon test code in which is unecessary, but temporary with the aim
    of getting up and running quickly.
    
    Change-Id: I5fdce186df09e1168e8d52abfec17e0c20a64031
    Partially-Implements: blueprint initial-setup

---
+++ README.rst
@@ -0,0 +1,51 @@
+===============================================================
+Cisco UI: Cisco Extension for the OpenStack Dashboard (Horizon)
+===============================================================
+
+* Release management: https://launchpad.net/horizon-cisco-ui
+* Blueprints and feature specifications: https://blueprints.launchpad.net/horizon-cisco-ui
+* Issue tracking: https://bugs.launchpad.net/horizon-cisco-ui
+
+Cisco UI is a Horizon Dashboard for interacting with Cisco Systems hardware.
+It uses the standard Horizon extension systems, and maintains code and styling
+consistency where possible.
+
+Most of the developer information, as well as an overview of Horizon, can be
+found in the `Horizon documentation online`_.
+
+.. _Horizon documentation online: http://docs.openstack.org/developer/horizon/index.html
+
+Getting Started
+===============
+
+The quickest way to get up and running is:
+
+  1. Setup a basic `Devstack installation`_
+  2. Clone `Cisco UI`_ with ``git clone https://github.com/openstack/horizon-cisco-ui``
+  3. Enter the ``horizon-cisco-ui`` directory, and run ``./dev_install.sh``.
+     Follow the on screen instructions. Often the default settings will be
+     adequate, so you can just hit enter twice.
+
+.. _Devstack installation: http://docs.openstack.org/developer/devstack/
+.. _Cisco UI: https://github.com/openstack/horizon-cisco-ui
+
+Building Documentation
+======================
+
+This documentation is written by contributors, for contributors.
+
+The source is maintained in the ``doc/source`` directory using
+`reStructuredText`_ and built by `Sphinx`_
+
+.. _reStructuredText: http://docutils.sourceforge.net/rst.html
+.. _Sphinx: http://sphinx-doc.org/
+
+* Building Automatically::
+
+    $ ./run_tests.sh --docs
+
+* Building Manually::
+
+    $ tools/with_venv.sh sphinx-build doc/source doc/build/html
+
+Results are in the ``doc/build/html`` directory
---
+++ dev_install.sh
@@ -0,0 +1,131 @@
+#!/bin/sh
+
+# Small script for installing Horizon plugins
+# Defaults to Horizon IS installed at opt/stack/horizon
+
+### Variables
+
+# Change these for your specific plugin
+DASHBOARD_FILE="horizon_cisco_ui/enabled/_6000_cisco.py" # Relative path
+N1K_PANEL_FILE="horizon_cisco_ui/enabled/_6010_nexus1000v.py" # Relative path
+
+# These are defaults, may be useful to change but can normally be left alone
+HORIZON_DIR="/opt/stack/horizon"
+
+### Functions
+
+function setup_horizon {
+  echo "Is Horizon installed? [Y/n] ?"
+
+  read INSTALLED
+  
+  case $INSTALLED in
+    [yY]|"")
+      set_directory
+      ;;
+    [nN])
+      STACK_DIR="/opt/stack"
+      echo "Checking for presence of $STACK_DIR..."
+      if [ ! -d "$STACK_DIR" ] ; then
+        echo "$STACK_DIR not found. Creating (requires sudo)..."
+        sudo mkdir -p $STACK_DIR
+        sudo chown -R $USER $STACK_DIR
+        chmod 0755 $STACK_DIR
+      fi
+
+      echo "Checking for git..."
+      if command -v git >/dev/null 2>&1 ; then
+        git clone https://github.com/openstack/horizon.git /opt/stack/horizon
+      else
+        echo "Git is not available, but is required. Please install git."
+        exit 1
+      fi
+      ;;
+    *)
+      echo "Input not recognised"
+      setup_horizon
+      ;;
+  esac
+}
+
+function set_directory {
+  echo "Please enter your Horizon root directory. Leave blank for default [$HORIZON_DIR]:"
+
+  read CUSTOM_DIR
+
+  if [ ! -z "${CUSTOM_DIR// }" ]  ; then
+    if [[ $CUSTOM_DIR == /*/ ]] ; then
+      HORIZON_DIR=$CUSTOM_DIR
+    elif [[ $CUSTOM_DIR == */ ]] ; then
+      HORIZON_DIR=/$CUSTOM_DIR
+    elif [[ $CUSTOM_DIR == /* ]] ; then
+      HORIZON_DIR=$CUSTOM_DIR/
+    else
+      HORIZON_DIR=/$CUSTOM_DIR/
+    fi
+  fi
+}
+
+function copy_enabled_file {
+  ENABLED_DIR=$HORIZON_DIR"/openstack_dashboard/enabled/"
+
+  if [ -d "$ENABLED_DIR" ] ; then
+    echo "\nCopying $DASHBOARD_FILE to $ENABLED_DIR..."
+    if cp $DASHBOARD_FILE $ENABLED_DIR ; then
+      echo "Copy succeeded"
+    else
+      echo "Copy failed"
+      exit 1
+    fi
+  fi
+
+  if [ -d "$ENABLED_DIR" ] ; then
+    echo "\nCopying $N1K_PANEL_FILE to $ENABLED_DIR..."
+    if cp $N1K_PANEL_FILE $ENABLED_DIR ; then
+      echo "Copy succeeded"
+    else
+      echo "Copy failed"
+      exit 1
+    fi
+  fi
+}
+
+function install_plugin {
+  echo "\nChecking for existing venv..." 
+  if [ ! -d $HORIZON_DIR"/.venv" ] ; then
+    echo "No venv found"
+    python $HORIZON_DIR"/tools/install_venv.py"
+  else
+    echo "Existing venv found [$HORIZON_DIR/.venv]"
+  fi
+
+  echo "\nInstalling plugin..."
+  if $HORIZON_DIR/tools/with_venv.sh pip install -e . ; then
+    echo "Plugin installed"
+  else
+    echo "Plugin installation failed"
+    exit 1
+  fi
+}
+
+function launch_horizon {
+  cd $HORIZON_DIR
+
+  echo "\nChecking for settings file..."
+  if [ -f openstack_dashboard/local/local_settings.py ] ; then
+    echo "Settings file found [$PWD/openstack_dashboard/local/local_settings.py]"
+  else
+    echo "Settings file not found. Using default..."
+    cp openstack_dashboard/local/local_settings.py.example openstack_dashboard/local/local_settings.py
+  fi
+
+  echo "\nLaunching Horizon at 127.0.0.1:8080..."
+  ./run_tests.sh --runserver 127.0.0.1:8080
+}
+
+### Execution
+
+setup_horizon
+install_plugin
+copy_enabled_file
+launch_horizon
