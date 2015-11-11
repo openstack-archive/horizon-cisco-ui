eb29b1e7b5c9c855772cc51de3b23a526c6f79af
Rob Cresswell <robert.cresswell@outlook.com>
2015-11-11 09:06:12 +0000

    Make the plugin translatable
    This patch fixes the translation setup in the Cisco UI plugin
    
    Implements: blueprint add-translation
    Change-Id: I429f575c9cddd5caa61d160f969c6f4edd5a4978

--- manage.py
+++ manage.py
@@ -19,5 +19,5 @@ from django.core.management import execute_from_command_line  # noqa
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE",
-                          "horizon_cisco_ui.cisco.test.settings")
+                          "horizon_cisco_ui.test.settings")
     execute_from_command_line(sys.argv)
