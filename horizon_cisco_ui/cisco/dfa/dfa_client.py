5e6cfd24004c44f181e969234271c22e77853e74
chiragtayal <chiragtayal@gmail.com>
2016-10-18 23:46:02 -0700

    Programmable Fabric Panel in Cisco dashboard
    It displays Nexus Programmable Fabric information for troubleshooting
    and provide fabric summary, topology, network profile information
    
    Change-Id: I84e1d2b1b163104be5b84411056141aa3ada6f95

---
+++ horizon_cisco_ui/cisco/dfa/dfa_client.py
@@ -0,0 +1,207 @@
+# Copyright 2014 Cisco Systems, Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import json
+import logging
+import platform
+import sys
+
+from django.utils.translation import ugettext_lazy as _
+
+from networking_cisco.apps.saf.common import config
+from networking_cisco.apps.saf.common import constants
+from networking_cisco.apps.saf.common import rpc
+
+from horizon import exceptions
+from horizon.utils.memoized import memoized
+
+LOG = logging.getLogger(__name__)
+
+
+class DFAClient(object):
+    """Represents fabric enabler command line interface."""
+
+    def __init__(self):
+        self.ctl_host = platform.node()
+        self._cfg = config.CiscoDFAConfig().cfg
+        url = self._cfg.dfa_rpc.transport_url % ({'ip': self.ctl_host})
+        self.clnt = rpc.DfaRpcClient(url, constants.DFA_SERVER_QUEUE,
+                                     exchange=constants.DFA_EXCHANGE)
+
+    def do_precreate_network(self, network):
+        '''Precreate network on current version of Fabric Enabler'''
+
+        context = {}
+        args = json.dumps(network)
+        msg = self.clnt.make_msg('precreate_network', context, msg=args)
+        try:
+            resp = self.clnt.call(msg)
+            if not resp:
+                raise exceptions.NotAvailable("Project %(id)s not present in "
+                                              "fabric enabler" %
+                                              {'id': network.get('tenant_id')})
+            return resp
+        except (rpc.MessagingTimeout, rpc.RPCException, rpc.RemoteError):
+            LOG.error("RPC: Request to precreate network failed.")
+            raise exceptions.NotAvailable("RPC to Fabric Enabler failed")
+
+    def do_delete_precreate_network(self, network):
+        '''Delete precreate network on current version of Fabric Enabler'''
+
+        context = {}
+        args = json.dumps(network)
+        msg = self.clnt.make_msg('delete_precreate_network', context, msg=args)
+        try:
+            resp = self.clnt.call(msg)
+            if not resp:
+                raise exceptions.NotAvailable("Project %(id)s not present in "
+                                              "fabric enabler" %
+                                              {'id': network.get('tenant_id')})
+            return resp
+        except (rpc.MessagingTimeout, rpc.RPCException, rpc.RemoteError):
+            LOG.error("RPC: Request to delete precreated network failed.")
+            raise exceptions.NotAvailable("RPC to Fabric Enabler failed")
+
+    def do_associate_dci_id_to_project(self, tenant):
+        '''Associate DCI ID to Project'''
+
+        context = {}
+        args = json.dumps(tenant)
+        msg = self.clnt.make_msg('associate_dci_id_to_project',
+                                 context, msg=args)
+        try:
+            resp = self.clnt.cast(msg)
+            return resp
+        except (rpc.MessagingTimeout, rpc.RPCException, rpc.RemoteError):
+            LOG.error("RPC: Request to associate DCI_ID to Project failed.")
+            raise exceptions.NotAvailable("RPC to Fabric Enabler failed")
+
+    def get_fabric_summary(self):
+        '''Get fabric details from the Fabric Enabler'''
+
+        context = {}
+        args = json.dumps({})
+        msg = self.clnt.make_msg('get_fabric_summary', context, msg=args)
+        try:
+            resp = self.clnt.call(msg)
+            return resp
+        except (rpc.MessagingTimeout, rpc.RPCException, rpc.RemoteError):
+            LOG.error("RPC: Request for Fabric Summary failed.")
+            raise exceptions.NotAvailable("RPC to Fabric Enabler failed")
+
+    def get_config_profiles_detail(self):
+        '''Get all config Profiles details from the Fabric Enabler'''
+
+        context = {}
+        args = json.dumps({})
+        msg = self.clnt.make_msg('get_config_profiles_detail',
+                                 context, msg=args)
+        try:
+            resp = self.clnt.call(msg)
+            return resp
+        except Exception as e:
+            mess = (_('%(reason)s') % {"reason": e})
+            LOG.error(mess)
+            reason = mess.partition("Traceback")[0]
+            raise exceptions.NotAvailable(reason)
+
+    def get_project_details(self, tenant):
+        '''Get project details for a tenant from the Fabric Enabler'''
+
+        context = {}
+        args = json.dumps(tenant)
+        msg = self.clnt.make_msg('get_project_detail',
+                                 context, msg=args)
+        try:
+            resp = self.clnt.call(msg)
+            if not resp:
+                raise exceptions.NotFound("Project Not Found in Fabric \
+                                          Enabler")
+            return resp
+        except (rpc.MessagingTimeout, rpc.RPCException, rpc.RemoteError):
+            LOG.error("RPC: Request for project details failed.")
+            raise exceptions.NotAvailable("RPC to Fabric Enabler failed")
+
+    def get_network_by_tenant_id(self, tenant):
+        '''Get all networks for a tenant from the Fabric Enabler'''
+
+        context = {}
+        args = json.dumps(tenant)
+        msg = self.clnt.make_msg('get_all_networks_for_tenant',
+                                 context, msg=args)
+        try:
+            resp = self.clnt.call(msg)
+            if resp is False:
+                raise exceptions.NotFound("Project Not Found in Fabric \
+                                          Enabler")
+            return resp
+        except (rpc.MessagingTimeout, rpc.RPCException, rpc.RemoteError):
+            LOG.error("RPC: Request for project details failed.")
+            raise exceptions.NotAvailable("RPC to Fabric Enabler failed")
+
+    def get_instance_by_tenant_id(self, tenant):
+        '''Get all instances for a tenant from the Fabric Enabler'''
+
+        context = {}
+        args = json.dumps(tenant)
+        msg = self.clnt.make_msg('get_instance_by_tenant_id',
+                                 context, msg=args)
+        try:
+            resp = self.clnt.call(msg)
+            if resp is False:
+                raise exceptions.NotFound("Project Not Found in Fabric \
+                                          Enabler")
+            return resp
+        except (rpc.MessagingTimeout, rpc.RPCException, rpc.RemoteError):
+            LOG.error("RPC: Request for project details failed.")
+            raise exceptions.NotAvailable("RPC to Fabric Enabler failed")
+
+    def get_agents_details(self):
+        '''Get all Enabler agents from the Fabric Enabler'''
+
+        context = {}
+        args = json.dumps({})
+        msg = self.clnt.make_msg('get_agents_details', context, msg=args)
+        try:
+            resp = self.clnt.call(msg)
+            if not resp:
+                raise exceptions.NotFound("No Agents found for Fabric Enabler")
+            return resp
+        except (rpc.MessagingTimeout, rpc.RPCException, rpc.RemoteError):
+            LOG.error("RPC: Request for project details failed.")
+            raise exceptions.NotAvailable("RPC to Fabric Enabler failed")
+
+    def get_agent_details_per_host(self, agent):
+        '''Get Enabler agent for a host from the Fabric Enabler'''
+
+        context = {}
+        args = json.dumps(agent)
+        msg = self.clnt.make_msg('get_agent_details_per_host', context,
+                                 msg=args)
+        try:
+            resp = self.clnt.call(msg)
+            if not resp:
+                raise exceptions.NotFound("No Agent found for Fabric Enabler")
+            return resp
+        except (rpc.MessagingTimeout, rpc.RPCException, rpc.RemoteError):
+            LOG.error("RPC: Request for project details failed.")
+            raise exceptions.NotAvailable("RPC to Fabric Enabler failed")
+
+
+@memoized
+def dfa_client():
+    sys.argv.append('--config-file')
+    sys.argv.append('/etc/saf/enabler_conf.ini')
+    return DFAClient()
